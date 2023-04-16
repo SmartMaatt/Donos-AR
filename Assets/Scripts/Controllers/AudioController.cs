8706f08cfc802a3ad9aaf9754740489cff283309
MultiPrice <igobud01@gmail.com>
2023-04-16 19:02:59 +0200

    Audio Manager
    * added audio manager to application
    * added police audio clips

---
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
